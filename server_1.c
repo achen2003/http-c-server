b527b553bf73ea376cbc7afdf93641c59099c028
Alex Chen <alexchen.chen@mail.utoronto.ca>
2023-12-21 17:44:33 -0500

    Starting work on first iteration of the server, added include statements

---
+++ server_1.c
@@ -0,0 +1,10 @@
+#include <assert.h>
+#include <errno.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
