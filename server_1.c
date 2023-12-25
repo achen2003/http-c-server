0c75a459988b47675e983b949d53720c8ec874ff
Alex Chen <alexchen.chen@mail.utoronto.ca>
2023-12-25 14:27:34 -0500

    Added logging feature which saves logs to a plain text file

--- server_1.c
+++ server_1.c
@@ -11,18 +11,37 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <netinet/in.h>
+#include <stdarg.h>
+#include <time.h>
+
+/*========== Global variables and data structures ==========*/
 
 #define SOCKET_PATH "example.sock"
 #define PORT_NUMBER 12345
 
 static int fd;
+
 static pthread_mutex_t client_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+static FILE *log_file = NULL;
 
 typedef struct {
     int client_fd;
 } ThreadData;
 
-/*===================================================================*/
+typedef enum {
+    LOG_INFO,
+    LOG_DEBUG,
+    LOG_WARNING,
+    LOG_ERROR
+} LogLevel;
+
+typedef struct {
+    LogLevel log_level;
+    const char *log_file_path;
+} ServerConfig;
+
+/*========== Error checking functions ==========*/
 
 void check(int ret, const char* message) {
     // Function for error checking
@@ -33,6 +52,58 @@ void check(int ret, const char* message) {
     exit(errno);
 }
 
+/*========== Logging functions ==========*/
+
+const char *log_level_str(LogLevel level) {
+    switch (level) {
+        case LOG_INFO: return "INFO";
+        case LOG_DEBUG: return "DEBUG";
+        case LOG_WARNING: return "WARNING";
+        case LOG_ERROR: return "ERROR";
+        default: return "UNKNOWN";
+    }
+}
+
+void log_message(LogLevel level, const char *message, ...) {
+    if (log_file == NULL) {
+        fprintf(stderr, "Error - log file not initialized\n");
+        return;
+    }
+
+    va_list args;
+    va_start(args, message);
+
+    time_t rawtime;
+    struct tm* timeinfo;
+    time(&rawtime);
+    timeinfo = localtime(&rawtime);
+
+    fprintf(log_file, "[%s] %04d-%02d-%02d %02d:%02d:%02d ",
+            log_level_str(level), timeinfo->tm_year + 1900, timeinfo->tm_mon + 1,
+            timeinfo->tm_mday, timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
+    vfprintf(log_file, message, args);
+    fprintf(log_file, "\n");
+
+    va_end(args);
+}
+
+void init_log_file(const char *file_path) {
+    log_file = fopen(file_path, "a");
+    if (log_file == NULL) {
+        fprintf(stderr, "Error: Could not open log file '%s'\n", file_path);
+        exit(EXIT_FAILURE);
+    }
+}
+
+void close_log_file() {
+    if (log_file != NULL) {
+        fclose(log_file);
+        log_file = NULL;
+    }
+}
+
+/*========== Main server functions ==========*/
+
 void close_socket() {
     check(close(fd), "Error - close()");
     // check(unlink(SOCKET_PATH), "Error - unlink()");
@@ -41,6 +112,7 @@ void close_socket() {
 
 void handle_signal(int signum) {
     assert(signum == SIGINT || signum == SIGTERM);
+    close_log_file();
     close_socket();
 }
 
@@ -72,6 +144,8 @@ void* handle_client(void *arg) {
 
     check(close(client_fd), "Error - close()");
 
+    log_message(LOG_INFO, "Client request processes successfully");
+
     pthread_mutex_unlock(&client_mutex);
 
     free(thread_data);
@@ -82,6 +156,8 @@ void* handle_client(void *arg) {
 int main(void) {
     register_signal(SIGINT);
     register_signal(SIGTERM);
+
+    init_log_file("/home/alex/http-c-server/logs/server_logs.txt");
 
     // Set up socket
     fd = socket(AF_INET, SOCK_STREAM, 0);
